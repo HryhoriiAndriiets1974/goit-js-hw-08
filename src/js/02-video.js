e9f72fa3b3011e82aaca1814876b7f82749e3630
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-19 15:28:45 +0300

    hw-08-js-video-branch 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+let throttle = require('lodash.throttle');
+import vimeoPlayer from '@vimeo/player';
 
+const player = new vimeoPlayer(document.querySelector('#vimeo-player'));
+const getTime = localStorage.getItem('videoplayer-current-time');
+
+//  ========================================== branch 1
+player.on('timeupdate', throttle(e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+  console.log(e.seconds);}, 1000),);
+
+player.setCurrentTime(getTime).catch(function (error) {
+switch (error.name) {
+  case 'RangeError':
+    // the time was less than 0 or greater than the video’s duration
+    break;
+  default:
+    // some other error occurred
+    break;}
+});
+// ========================================== branch 2
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// if (getTime) {player.setCurrentTime(Number.parseFloat(getTime))};
+
+// function onPlay(e) {localStorage.setItem('videoplayer-current-time',
+//  e.seconds === e.duration ? 0 : e.seconds);
+//  console.log(e.seconds);};
